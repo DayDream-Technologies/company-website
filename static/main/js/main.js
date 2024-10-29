27c9b7dbbd11ad2222427b663ef3c2f1c12f3f39
SpaceFishyyy <tunagumustas@gmail.com>
2024-10-29 15:05:47 -0400

    easy team member adding

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -11,34 +11,9 @@ const teamMembers = [
         linkedin: "https://www.linkedin.com/company/daydream-technologies/",
         image: "../static/main/images/default_pic.jpg"
     },
-    {
-        name: "Member 1",
-        role: "Developer",
-        linkedin: "https://www.linkedin.com/company/daydream-technologies/",
-        image: "../static/main/images/default_pic.jpg"
-    },
-    {
-        name: "Member 2",
-        role: "Designer",
-        linkedin: "https://www.linkedin.com/company/daydream-technologies/",
-        image: "../static/main/images/default_pic.jpg"
-    },
-    {
-        name: "Member 1",
-        role: "Developer",
-        linkedin: "https://www.linkedin.com/company/daydream-technologies/",
-        image: "../static/main/images/default_pic.jpg"
-    },
-    {
-        name: "Member 2",
-        role: "Designer",
-        linkedin: "https://www.linkedin.com/company/daydream-technologies/",
-        image: "../static/main/images/default_pic.jpg"
-    },
-    // Add more team members here
+    // Add more team members as needed
 ];
 
-// Function to render team members on the team page
 function renderTeamMembers() {
     const teamContainer = document.getElementById('team-container');
     teamContainer.innerHTML = '';  // Clear existing members (if any)
@@ -48,11 +23,16 @@ function renderTeamMembers() {
         memberDiv.classList.add('team-member');
 
         memberDiv.innerHTML = `
-            <a target="_blank" href="${member.linkedin}" title="${member.name}'s LinkedIn">
-                <img src="${member.image}" alt="${member.name}">
-            </a>
-            <h3>${member.name}</h3>
-            <p>Role: ${member.role}</p>
+            <div class="card">
+                <div class="card-front">
+                    <img src="${member.image}" alt="${member.name}">
+                    <h3>${member.name}</h3>
+                    <p>Role: ${member.role}</p>
+                </div>
+                <div class="card-back">
+                    <a href="${member.linkedin}" target="_blank">LinkedIn Profile</a>
+                </div>
+            </div>
         `;
         
         teamContainer.appendChild(memberDiv);
